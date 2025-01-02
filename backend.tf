c0a9c2bbb23fa416194fbf8973ee294f9e966c16
Harilal Bhaskaran <5723929+harilalbhaskaran@users.noreply.github.com>
2025-01-02 16:37:38 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.1.0"
   backend "remote" {
     hostname = "app.terraform.io"
-    # organization = "<replace-with-your-Terraform-Cloud-organization-and-uncomment>"
+    organization = "harilal-bhaskaran"
     workspaces {
       name = "ace-iac-day-zero"
     }
